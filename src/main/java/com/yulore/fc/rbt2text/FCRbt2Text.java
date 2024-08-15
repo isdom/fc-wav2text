65447e0ee68cef7b4a2469a1654e7731e5108931
ming.ma <ming.ma@yulore.com>
2024-08-15 14:30:20 +0800

    add Redis Access Support

--- src/main/java/com/yulore/fc/rbt2text/FCRbt2Text.java
+++ src/main/java/com/yulore/fc/rbt2text/FCRbt2Text.java
@@ -7,85 +7,73 @@ import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.model.OSSObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import org.asynchttpclient.AsyncHttpClient;
+import org.jetbrains.annotations.NotNull;
 
 import static org.asynchttpclient.Dsl.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
 
 public class FCRbt2Text implements PojoRequestHandler<RbtEvent[], String> {
-
-    private void sendRbtResult(final Context context, final RbtResultVO vo, Channel channel, final String exchange, final String routingKey) throws IOException {
-        final String json = new ObjectMapper().writeValueAsString(vo);
-        channel.basicPublish(exchange, routingKey, null, json.getBytes(StandardCharsets.UTF_8));
-        context.getLogger().info("send to mq:" + json);
-    }
-
     @Override
     public String handleRequest(final RbtEvent[] events, final Context context) {
-        final String endpoint = System.getenv("RBT_OSS_ENDPOINT");
+        try (final RedisClient redisClient = RedisClient.create(getRedisURI());
+             final StatefulRedisConnection<String, String> redisConnection = redisClient.connect();
+             final AsyncHttpClient ahc = asyncHttpClient();
+             final com.rabbitmq.client.Connection rabbitmqConnection = getRabbitMQConnectionFactory(context).newConnection();
+             final com.rabbitmq.client.Channel rabbitmqChannel = rabbitmqConnection.createChannel()) {
 
-        // 获取密钥信息，执行前，确保函数所在的服务配置了角色信息，并且角色需要拥有 AliyunOSSFullAccess权限
-        // 建议直接使用 AliyunFCDefaultRole 角色
-        final Credentials creds = context.getExecutionCredentials();
+            //final RedisAsyncCommands<String, String> redisCommands = redisConnection.async();
+            context.getLogger().warn("func: (FunctionName:" + context.getFunctionParam().getFunctionName()
+                    + "/FunctionInitializer:" + context.getFunctionParam().getFunctionInitializer()
+                    + "/ServiceName:" + context.getService().getName() +")");
+            // redisCommands.hset();
 
-        final ConnectionFactory factory = new ConnectionFactory();
-        final String uri = System.getenv("RBT_MQ_URI");
-        final String exchange= System.getenv("RBT_MQ_EXCHANGE");
-        final String routingKey = System.getenv("RBT_MQ_ROUTINGKEY");
-        try {
-            factory.setUri(uri);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        try (final AsyncHttpClient ahc = asyncHttpClient();
-             final Connection conn = factory.newConnection();
-             final Channel channel = conn.createChannel()) {
-            context.getLogger().info("AHC config:" + ahc.getConfig());
-            context.getLogger().info("MQ env: uri:" + uri + ", exchange:" + exchange + ",routingKey:" + routingKey);
-            // 创建OSSClient实例。
-            final OSS ossClient = new OSSClientBuilder().build(endpoint,
-                    creds.getAccessKeyId(),
-                    creds.getAccessKeySecret(),
-                    creds.getSecurityToken());
             context.getLogger().info("handle (" + events.length + ") events");
+            context.getLogger().info("AHC config:" + ahc.getConfig());
 
             final CountDownLatch finishLatch = new CountDownLatch(events.length);
 
-            for (RbtEvent event : events) {
-                final RbtResultVO resultvo = new RbtResultVO();
-                resultvo.setSessionId(event.data.body.getSessionId());
-                resultvo.setSourceTimestamp(event.data.body.getSourceTimestamp());
+            // 创建OSSClient实例。
+            // 获取密钥信息，执行前，确保函数所在的服务配置了角色信息，并且角色需要拥有 AliyunOSSFullAccess权限
+            // 建议直接使用 AliyunFCDefaultRole 角色
+            final OSS ossClient = getOssClient(context.getExecutionCredentials());
 
-                final OSSObject source = getOSSObject(context, event, ossClient, resultvo);
+            final Consumer<RbtResultVO> sendResult = buildSendRbtResultVO(context, rabbitmqChannel);
+
+            for (RbtEvent event : events) {
+                final RbtResultVO rrvo = new RbtResultVO();
+                rrvo.setSessionId(event.data.body.getSessionId());
+                rrvo.setSourceTimestamp(event.data.body.getSourceTimestamp());
+
+                final OSSObject source = getOSSObject(context, event, ossClient, rrvo);
                 if (source == null) {
                     finishLatch.countDown();
                     continue;
                 }
                 try {
                     context.getLogger().info("oss source: " + source.getKey());
-                    resultvo.setStartProcessTimestamp(System.currentTimeMillis());
+                    rrvo.setStartProcessTimestamp(System.currentTimeMillis());
                     new FunasrClient(context,
                             ahc.prepareGet(System.getenv("FUNASR_WSURI")),
                             source.getObjectContent(),
                             (text) -> {
-                                resultvo.setEndProcessTimestamp(System.currentTimeMillis());
-                                resultvo.setText(text);
+                                rrvo.setEndProcessTimestamp(System.currentTimeMillis());
+                                rrvo.setText(text);
                                 try {
                                     source.close();
                                 } catch (IOException e) {
                                     // throw new RuntimeException(e);
                                 }
-                                try {
-                                    sendRbtResult(context, resultvo, channel, exchange, routingKey);
-                                } catch (IOException e) {
-                                    // throw new RuntimeException(e);
-                                }
+                                sendResult.accept(rrvo);
                                 finishLatch.countDown();
                             });
                 } catch (Exception e) {
@@ -105,6 +93,59 @@ public class FCRbt2Text implements PojoRequestHandler<RbtEvent[], String> {
         }
 
         return "handle (" + events.length +") events";
+    }
+
+    private @NotNull Consumer<RbtResultVO> buildSendRbtResultVO(final Context context,
+                                                                final com.rabbitmq.client.Channel rabbitmqChannel) {
+        final String exchange= System.getenv("RBT_MQ_EXCHANGE");
+        final String routingKey = System.getenv("RBT_MQ_ROUTINGKEY");
+
+        context.getLogger().info("MQ env: exchange:" + exchange + ",routingKey:" + routingKey);
+        return (vo) -> {
+            try {
+                sendRbtResult(context, vo, rabbitmqChannel, exchange, routingKey);
+            } catch (IOException e) {
+                // throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private static @NotNull RedisURI getRedisURI() {
+        return RedisURI.Builder.redis(System.getenv("RBT_REDIS_HOST"))
+                .withPassword(System.getenv("RBT_REDIS_PASSWD").toCharArray())
+                .withPort(Integer.parseInt(System.getenv("RBT_REDIS_PORT")))
+                .withDatabase(Integer.parseInt(System.getenv("RBT_REDIS_DB")))
+                .build();
+    }
+
+    private void sendRbtResult(final Context context,
+                               final RbtResultVO vo,
+                               final com.rabbitmq.client.Channel rabbitmqChannel,
+                               final String exchange,
+                               final String routingKey) throws IOException {
+        final String json = new ObjectMapper().writeValueAsString(vo);
+        rabbitmqChannel.basicPublish(exchange, routingKey, null, json.getBytes(StandardCharsets.UTF_8));
+        context.getLogger().info("send to rabbitmq:" + json);
+    }
+
+    private static @NotNull ConnectionFactory getRabbitMQConnectionFactory(final Context context) {
+        final ConnectionFactory factory = new ConnectionFactory();
+        final String uri = System.getenv("RBT_MQ_URI");
+        try {
+            factory.setUri(uri);
+            context.getLogger().info("MQ env: uri:" + uri);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return factory;
+    }
+
+    private static OSS getOssClient(final Credentials creds) {
+        final String endpoint = System.getenv("RBT_OSS_ENDPOINT");
+        return new OSSClientBuilder().build(endpoint,
+                creds.getAccessKeyId(),
+                creds.getAccessKeySecret(),
+                creds.getSecurityToken());
     }
 
     private OSSObject getOSSObject(final Context context, final RbtEvent event, final OSS ossClient, final RbtResultVO resultvo) {
