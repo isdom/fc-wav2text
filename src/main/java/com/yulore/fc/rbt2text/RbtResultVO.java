1fe7139018f6ef24e159ea77b2f252530bf6684b
ming.ma <ming.ma@yulore.com>
2024-08-19 15:30:04 +0800

    add lombok support

--- src/main/java/com/yulore/fc/rbt2text/RbtResultVO.java
+++ src/main/java/com/yulore/fc/rbt2text/RbtResultVO.java
@@ -1,5 +1,12 @@
 package com.yulore.fc.rbt2text;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class RbtResultVO {
     String sessionId;
     String objectName;
@@ -7,64 +14,4 @@ public class RbtResultVO {
     long   sourceTimestamp;
     long   startProcessTimestamp;
     long   endProcessTimestamp;
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public String getObjectName() {
-        return objectName;
-    }
-
-    public void setObjectName(String objectName) {
-        this.objectName = objectName;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public long getSourceTimestamp() {
-        return sourceTimestamp;
-    }
-
-    public void setSourceTimestamp(long sourceTimestamp) {
-        this.sourceTimestamp = sourceTimestamp;
-    }
-
-    public long getStartProcessTimestamp() {
-        return startProcessTimestamp;
-    }
-
-    public void setStartProcessTimestamp(long startProcessTimestamp) {
-        this.startProcessTimestamp = startProcessTimestamp;
-    }
-
-    public long getEndProcessTimestamp() {
-        return endProcessTimestamp;
-    }
-
-    public void setEndProcessTimestamp(long endProcessTimestamp) {
-        this.endProcessTimestamp = endProcessTimestamp;
-    }
-
-    @Override
-    public String toString() {
-        return "RbtResultVO{" +
-                "sessionId='" + sessionId + '\'' +
-                ", objectName='" + objectName + '\'' +
-                ", text='" + text + '\'' +
-                ", sourceTimestamp=" + sourceTimestamp +
-                ", startProcessTimestamp=" + startProcessTimestamp +
-                ", endProcessTimestamp=" + endProcessTimestamp +
-                '}';
-    }
 }
