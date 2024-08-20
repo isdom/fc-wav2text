4dbfb1bea42b03ef87bc3f9484246149c94eefe8
ming.ma <ming.ma@yulore.com>
2024-08-20 17:40:04 +0800

    add lombok support

--- src/main/java/com/yulore/fc/rbt2text/RbtEvent.java
+++ src/main/java/com/yulore/fc/rbt2text/RbtEvent.java
@@ -2,24 +2,13 @@ package com.yulore.fc.rbt2text;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yulore.fc.pojo.CloudEvent;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@ToString
 public class RbtEvent extends CloudEvent {
     RbtData data;
-
-    @JsonProperty("data")
-    public RbtData getData() {
-        return data;
-    }
-
-    @JsonProperty("data")
-    public void setData(RbtData data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "RbtEvent{" +
-                "data=" + data +
-                "} " + super.toString();
-    }
 }
