b737e5d3847e8f18d864b943e5272b3965bc1bf7
ming.ma <ming.ma@yulore.com>
2024-08-22 18:19:03 +0800

    using lombok @Setter & @Getter

--- src/main/java/com/yulore/fc/rbt2text/RbtData.java
+++ src/main/java/com/yulore/fc/rbt2text/RbtData.java
@@ -1,45 +1,18 @@
 package com.yulore.fc.rbt2text;
 
 import com.yulore.fc.pojo.RabbitMQData;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 public class RbtData extends RabbitMQData {
+    @Setter
+    @Getter
+    @ToString
     static class Body {
         String sessionId;
         String ossPath;
         Long   sourceTimestamp;
-
-        public Long getSourceTimestamp() {
-            return sourceTimestamp;
-        }
-
-        public void setSourceTimestamp(Long sourceTimestamp) {
-            this.sourceTimestamp = sourceTimestamp;
-        }
-
-        public String getSessionId() {
-            return sessionId;
-        }
-
-        public void setSessionId(String sessionId) {
-            this.sessionId = sessionId;
-        }
-
-        public String getOssPath() {
-            return ossPath;
-        }
-
-        public void setOssPath(String ossPath) {
-            this.ossPath = ossPath;
-        }
-
-        @Override
-        public String toString() {
-            return "Body{" +
-                    "sessionId='" + sessionId + '\'' +
-                    ", ossPath='" + ossPath + '\'' +
-                    ", sourceTimestamp=" + sourceTimestamp +
-                    '}';
-        }
     }
 
     Body body;
