fc000540ed4a12fc1ca25c25b06d2b728ba043e4
ming.ma <ming.ma@yulore.com>
2024-10-10 17:21:11 +0800

    optimize code

--- src/main/java/com/yulore/fc/wav2text/FunasrClient.java
+++ src/main/java/com/yulore/fc/wav2text/FunasrClient.java
@@ -102,7 +102,7 @@ public class FunasrClient {
         final int RATE = 8000;
         final String[] chunkList = strChunkSize.split(",");
         final int int_chunk_size = 60 * Integer.parseInt(chunkList[1].trim()) / chunkInterval;
-        int CHUNK = RATE / 1000 * int_chunk_size;
+        final int CHUNK = RATE / 1000 * int_chunk_size;
         int stride = 60 * Integer.parseInt(chunkList[1].trim()) / chunkInterval / 1000 * RATE * 2;
         log.info("chunk_size:{}", int_chunk_size);
         log.info("CHUNK:{}", CHUNK);
